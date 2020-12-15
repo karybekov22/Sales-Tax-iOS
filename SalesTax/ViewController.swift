e2601065d0f7cb6f58564704cd59f5bec6ed9b8d
Nursultan Karybekov <n.karybekov22@gmail.com>
2020-12-15 16:02:37 +0600

    Initial Commit

---
+++ SalesTax/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalesTax
+//
+//  Created by Nursultan Karybekov on 15/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
